f8920b0e641ccc6fae409cab056a806c30caf056
wchorski <wchorski@gmail.com>
2025-06-11 14:29:55 -0500

    before i move to combined svg

--- toolbar.js
+++ toolbar.js
@@ -1,9 +1,15 @@
 const inputColorTop = document.getElementById("colorpicker-top")
 const inputColorBottom = document.getElementById("colorpicker-bottom")
+
 const inputBrowTransformY = document.getElementById("brow-transform-y")
 const inputBrowTransformX = document.getElementById("brow-transform-x")
 const inputBrowScale = document.getElementById("brow-scale")
 const inputBrowRotate = document.getElementById("brow-transform-rotate")
+
+const inputEyeTransformY = document.getElementById("eye-transform-y")
+const inputEyeTransformX = document.getElementById("eye-transform-x")
+const inputEyeScale = document.getElementById("eye-scale")
+const inputEyeRotate = document.getElementById("eye-transform-rotate")
 
 function main() {
 	inputColorTop.addEventListener("input", (e) => {
@@ -12,6 +18,7 @@ function main() {
 	inputColorBottom.addEventListener("input", (e) => {
 		document.documentElement.style.setProperty("--c-bottom", e.target.value)
 	})
+  //? Brows
 	inputBrowTransformY.addEventListener("input", (e) => {
 		document.documentElement.style.setProperty(
 			"--brow-y-offset",
@@ -33,6 +40,32 @@ function main() {
 	inputBrowRotate.addEventListener("input", (e) => {
 		document.documentElement.style.setProperty(
 			"--brow-rotation",
+			`${(e.target.value * 180)}deg`
+		)
+	})
+
+  //? Eyes
+	inputEyeTransformY.addEventListener("input", (e) => {
+		document.documentElement.style.setProperty(
+			"--eye-y-offset",
+			`${(e.target.value * 28)}px`
+		)
+	})
+	inputEyeTransformX.addEventListener("input", (e) => {
+		document.documentElement.style.setProperty(
+			"--eye-x-offset",
+			`${(e.target.value * 20)}px`
+		)
+	})
+	inputEyeScale.addEventListener("input", (e) => {
+		document.documentElement.style.setProperty(
+			"--eye-scale",
+			`${(e.target.value * 1.3)}`
+		)
+	})
+	inputEyeRotate.addEventListener("input", (e) => {
+		document.documentElement.style.setProperty(
+			"--eye-rotation",
 			`${(e.target.value * 180)}deg`
 		)
 	})
